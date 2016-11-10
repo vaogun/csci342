036446f67caedac072831ecb07694e8dae732956
Levi Barker <Levi.barker@outlook.com>
2016-11-10 16:29:35 -0600

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+    CSCI 342 - Project 3: Pricess
+    main.cpp
+
+    @author Levi Barker
+    @contributors Matthew Kluzak, Victor Ogundipe
+    @version 1.0 11/10/16
+*/
+
+int main(){
+
+  return 0;
+}
