b7372becf0cb5c36a88512eed0fc54c65fb76cff
Levi Barker <Levi.barker@outlook.com>
2016-11-10 20:43:28 -0600

    added file input library

--- main.cpp
+++ main.cpp
@@ -7,7 +7,21 @@
     @version 1.0 11/10/16
 */
 
-int main(){
+#include <cstdio>
+using namespace std;
 
+const int maxString = 1024; // read buffer size
+
+int main( int argc, char ** argv){
+    const char * fileName = "map.txt"; // The input file string
+  
+    char buffer[maxString];
+    FILE * fileForReading = fopen(fileName, "r"); // Open file (fileName) for reading ("r")
+    
+    while(fgets(buffer, maxString, fileForReading)){
+        fputs(buffer, stdout); //TODO: Change this to input into structures
+    }
+    
+    fclose(fileForReading); // Close file
   return 0;
 }
