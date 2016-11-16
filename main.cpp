b428ae03c8fe45b575c1ae395bcd4d0e253e61ad
Levi Barker <Levi.barker@outlook.com>
2016-11-16 15:26:18 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 /**
     CSCI 342 - Project 3: Pricess
     main.cpp
-
-    @author Levi Barker
-    @contributors Matthew Kluzak, Victor Ogundipe
+    
+    @contributors Matthew Kluzak, Victor Ogundipe, Levi Barker
     @version 1.0 11/10/16
 */
 
